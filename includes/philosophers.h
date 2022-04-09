557c47c02f78c2097caa8891832a36e186520b63
Kotaro Kamashima <kotaro0726@gmail.com>
2022-04-10 08:26:44 +0900

    Update: Add basic header files

---
+++ includes/philosophers.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philosophers.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/10 08:22:35 by kkamashi          #+#    #+#             */
+/*   Updated: 2022/04/10 08:25:23 by kkamashi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILOSOPHERS_H
+# define PHILOSOPHERS_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <pthread.h>
+
+#endif
