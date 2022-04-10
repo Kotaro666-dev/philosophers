2a6f12d43ff1960f9dd132fbfff1a9b587ee0d02
Kotaro Kamashima <kotaro0726@gmail.com>
2022-04-10 20:46:10 +0900

    Update: Add log.c

--- includes/philosophers.h
+++ includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 08:22:35 by kkamashi          #+#    #+#             */
-/*   Updated: 2022/04/10 08:25:23 by kkamashi         ###   ########.fr       */
+/*   Updated: 2022/04/10 20:44:15 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,15 @@
 # include <string.h>
 # include <stdlib.h>
 # include <pthread.h>
+
+/*
+** LOG.C
+*/
+
+void	log_taking_fork(long timestamp_in_ms, int philosopher_number);
+void	log_eating(long timestamp_in_ms, int philosopher_number);
+void	log_sleeping(long timestamp_in_ms, int philosopher_number);
+void	log_thinking(long timestamp_in_ms, int philosopher_number);
+void	log_having_died(long timestamp_in_ms, int philosopher_number);
 
 #endif
