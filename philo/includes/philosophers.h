90101461e895bc3751b93f764d2ea0952dff2f72
Kotaro Kamashima <kotaro0726@gmail.com>
2022-04-23 16:24:04 +0900

    Update: Add error log function

--- philo/includes/philosophers.h
+++ philo/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 08:22:35 by kkamashi          #+#    #+#             */
-/*   Updated: 2022/04/10 20:44:15 by kkamashi         ###   ########.fr       */
+/*   Updated: 2022/04/23 16:17:29 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,16 @@
 # include <string.h>
 # include <stdlib.h>
 # include <pthread.h>
+# include <limits.h>
+
+typedef struct	s_philo
+{
+	int	number_of_philos;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	number_of_must_eat;
+}				t_philo;
 
 /*
 ** LOG.C
@@ -28,5 +38,21 @@ void	log_eating(long timestamp_in_ms, int philosopher_number);
 void	log_sleeping(long timestamp_in_ms, int philosopher_number);
 void	log_thinking(long timestamp_in_ms, int philosopher_number);
 void	log_having_died(long timestamp_in_ms, int philosopher_number);
+
+/*
+** HANDLE_ERROR.C
+*/
+
+void	perror_and_exit(char *str);
+
+/*
+** UTILITIES
+*/
+
+size_t	ft_strlen(const char *str);
+void	ft_putstr_fd(char *str, int fd);
+void	ft_putendl_fd(char *str, int fd);
+int		ft_atoi(const char *str);
+
 
 #endif
