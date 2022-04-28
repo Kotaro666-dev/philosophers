a68e3303fab74d2070ed6af56f548efa1c52606e
Kotaro Kamashima <kotaro0726@gmail.com>
2022-04-28 21:28:10 +0900

    Update: Add newline

--- philo/srcs/log.c
+++ philo/srcs/log.c
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 20:37:05 by kkamashi          #+#    #+#             */
-/*   Updated: 2022/04/27 09:17:41 by kkamashi         ###   ########.fr       */
+/*   Updated: 2022/04/28 21:26:22 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	log_taking_fork(int philosopher_number)
 	long	timestamp;
 
 	timestamp = get_current_timestamp();
-	printf("%ld %d has taken a fork", timestamp, philosopher_number);
+	printf("%ld %d has taken a fork\n", timestamp, philosopher_number);
 }
 
 void	log_eating(int philosopher_number)
@@ -25,7 +25,7 @@ void	log_eating(int philosopher_number)
 	long	timestamp;
 
 	timestamp = get_current_timestamp();
-	printf("%ld %d is eating", timestamp, philosopher_number);
+	printf("%ld %d is eating\n", timestamp, philosopher_number);
 }
 
 void	log_sleeping(int philosopher_number)
@@ -33,7 +33,7 @@ void	log_sleeping(int philosopher_number)
 	long	timestamp;
 
 	timestamp = get_current_timestamp();
-	printf("%ld %d is sleeping", timestamp, philosopher_number);
+	printf("%ld %d is sleeping\n", timestamp, philosopher_number);
 }
 
 void	log_thinking(int philosopher_number)
@@ -41,7 +41,7 @@ void	log_thinking(int philosopher_number)
 	long	timestamp;
 
 	timestamp = get_current_timestamp();
-	printf("%ld %d is thinking", timestamp, philosopher_number);
+	printf("%ld %d is thinking\n", timestamp, philosopher_number);
 }
 
 void	log_having_died(int philosopher_number)
@@ -49,5 +49,5 @@ void	log_having_died(int philosopher_number)
 	long	timestamp;
 
 	timestamp = get_current_timestamp();
-	printf("%ld %d died", timestamp, philosopher_number);
+	printf("%ld %d died\n", timestamp, philosopher_number);
 }
