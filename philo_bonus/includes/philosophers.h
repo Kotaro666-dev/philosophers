a27a1b848b1c09b49003c8a21254e4b919aefb4e
Kotaro Kamashima <kotaro0726@gmail.com>
2022-05-02 11:31:42 +0900

    Update: Add utilities

--- philo_bonus/includes/philosophers.h
+++ philo_bonus/includes/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 08:22:35 by kkamashi          #+#    #+#             */
-/*   Updated: 2022/04/10 20:44:15 by kkamashi         ###   ########.fr       */
+/*   Updated: 2022/05/02 11:31:21 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,15 @@ void	log_eating(long timestamp_in_ms, int philosopher_number);
 void	log_sleeping(long timestamp_in_ms, int philosopher_number);
 void	log_thinking(long timestamp_in_ms, int philosopher_number);
 void	log_having_died(long timestamp_in_ms, int philosopher_number);
+
+/*
+** UTILITIES
+*/
+
+size_t	ft_strlen(const char *str);
+void	ft_putstr_fd(char *str, int fd);
+void	ft_putendl_fd(char *str, int fd);
+int		ft_atoi(const char *str);
+long	get_current_timestamp();
 
 #endif
