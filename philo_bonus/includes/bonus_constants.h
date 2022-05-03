b9ae9a3a29c8837a5c00c2417e157f57efe5c4dd
Kotaro Kamashima <kotaro0726@gmail.com>
2022-05-03 13:49:37 +0900

    Update: Add new semaphore filename

--- philo_bonus/includes/bonus_constants.h
+++ philo_bonus/includes/bonus_constants.h
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 08:25:28 by kkamashi          #+#    #+#             */
-/*   Updated: 2022/05/03 13:24:51 by kkamashi         ###   ########.fr       */
+/*   Updated: 2022/05/03 13:43:36 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@
 # define ARGC_WITH_OPTION 6
 
 # define SEMAPHORE_FORKS_NAME "/philo_bonus_forks"
+# define SAMAPHORE_IS_EATING_NAME "/philo_bonus_is_eating"
 
 # define TIME_TO_THINK 1
-# define TIME_TO_WAIT_BEFORE_DINING 200
 # define TIME_TO_DO_NEXT_MONITORING 10
 
 #endif
