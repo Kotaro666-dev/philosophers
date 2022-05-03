a7d229aa97c03c919eba57dcb675c14f11a312b4
Kotaro Kamashima <kotaro0726@gmail.com>
2022-05-03 13:26:53 +0900

    Modify: Modify logging function

--- philo_bonus/includes/bonus_constants.h
+++ philo_bonus/includes/bonus_constants.h
@@ -6,7 +6,7 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 08:25:28 by kkamashi          #+#    #+#             */
-/*   Updated: 2022/05/02 12:58:39 by kkamashi         ###   ########.fr       */
+/*   Updated: 2022/05/03 13:24:51 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,9 @@
 # define ARGC_WITH_OPTION 6
 
 # define SEMAPHORE_FORKS_NAME "/philo_bonus_forks"
+
+# define TIME_TO_THINK 1
+# define TIME_TO_WAIT_BEFORE_DINING 200
+# define TIME_TO_DO_NEXT_MONITORING 10
 
 #endif
--- philo_bonus/srcs/log.c
+++ philo_bonus/srcs/log.c
@@ -6,33 +6,48 @@
 /*   By: kkamashi <kkamashi@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/10 20:37:05 by kkamashi          #+#    #+#             */
-/*   Updated: 2022/05/02 12:56:27 by kkamashi         ###   ########.fr       */
+/*   Updated: 2022/05/03 13:25:33 by kkamashi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "bonus_philosophers.h"
 
-void	log_taking_fork(long timestamp_in_ms, int philosopher_number)
+void	log_taking_fork(int philosopher_number)
 {
-	printf("%ld %d has taken a fork", timestamp_in_ms, philosopher_number);
+	long	timestamp;
+
+	timestamp = get_current_timestamp();
+	printf("%ld %d has taken a fork\n", timestamp, philosopher_number);
 }
 
-void	log_eating(long timestamp_in_ms, int philosopher_number)
+void	log_eating(int philosopher_number)
 {
-	printf("%ld %d is eating", timestamp_in_ms, philosopher_number);
+	long	timestamp;
+
+	timestamp = get_current_timestamp();
+	printf("%ld %d is eating\n", timestamp, philosopher_number);
 }
 
-void	log_sleeping(long timestamp_in_ms, int philosopher_number)
+void	log_sleeping(int philosopher_number)
 {
-	printf("%ld %d is sleeping", timestamp_in_ms, philosopher_number);
+	long	timestamp;
+
+	timestamp = get_current_timestamp();
+	printf("%ld %d is sleeping\n", timestamp, philosopher_number);
 }
 
-void	log_thinking(long timestamp_in_ms, int philosopher_number)
+void	log_thinking(int philosopher_number)
 {
-	printf("%ld %d is thinking", timestamp_in_ms, philosopher_number);
+	long	timestamp;
+
+	timestamp = get_current_timestamp();
+	printf("%ld %d is thinking\n", timestamp, philosopher_number);
 }
 
-void	log_having_died(long timestamp_in_ms, int philosopher_number)
+void	log_having_died(int philosopher_number)
 {
-	printf("%ld %d died", timestamp_in_ms, philosopher_number);
+	long	timestamp;
+
+	timestamp = get_current_timestamp();
+	printf("%ld %d died\n", timestamp, philosopher_number);
 }
